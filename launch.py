53d717fb9f2191bd68aaa5b48e5f65ce315c2ac6
Abilay99 <38275700+Abilay99@users.noreply.github.com>
2020-12-25 17:41:49 +0600

    optimized from https://github.com/Abilay99/KazOpenCorpora

---
+++ launch.py
@@ -0,0 +1,15 @@
+from time import sleep
+import zzgotoApertium
+sleep(5)
+import zzgotoEditedApertium
+sleep(5)
+import zzgotoouttext
+sleep(5)
+import zzgototrain
+sleep(5)
+import zzgotokeywordsplaintext
+sleep(5)
+import zzgotoAbstracts
+sleep(5)
+import zzgotoexcel
+print("Complete!!! See directories testAbstracts, testKeywords and file keywords.xls ")
